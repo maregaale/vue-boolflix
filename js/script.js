fe26d096148a6977fd2bfb07b5097fe7d42ac97d
Alessandro Marega <ale_marega@hotmail.it>
2021-04-09 13:32:17 +0200

    aggiunto evento al click sul bottone con chiamata API, stampa in console

--- js/script.js
+++ js/script.js
@@ -2,9 +2,21 @@ const app = new Vue (
   {
     el: "#root",
     data: {
-
+      search: "",
     },
-    
-
+    methods: {
+      searchFilm: function () {
+        axios.get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "cd918788b3810512019e7d18b803e41d",
+            query: this.search,
+            language: "it-IT",
+          }
+        })
+          .then(function (reply) {
+            console.log(reply.data);
+          })
+      }
+    },
   }
 );
